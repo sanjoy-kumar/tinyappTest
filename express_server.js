dfa6048a62e05fa702c241ef440c48642300fc4e
Sanjoy Kumar Das <sanjoy.kumar.backup@gmail.com>
2021-12-11 21:50:50 -0500

    Filling Out the urls_index.ejs Template

--- express_server.js
+++ express_server.js
@@ -35,6 +35,13 @@ app.get("/set", (req, res) => {
   res.send(`a = ${a}`);
  });
 
+ app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
